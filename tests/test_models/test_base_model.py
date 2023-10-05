2aca1f3860941c36443f7068fb5268eab677ee87
O-01 <sa.an-s@yandex.com>
2023-10-05 17:30:01 -0500

    Added more unittests to test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ unittest module containing tests for BaseModel class """
 import unittest
+import pycodestyle
+from genericpath import exists
 from models.base_model import BaseModel
 
 
@@ -21,15 +23,34 @@ class TestBaseModel_init(unittest.TestCase):
         self.assertTrue(len(BaseModel.save.__doc__) > 0)
         self.assertTrue(len(BaseModel.to_dict.__doc__) > 0)
 
-    def test_base_model_with_argument(self):
-        with self.assertRaises(TypeError):
-            BaseModel(1)
-
-    def test_base_model_datetime_attributes(self):
-        self.assertEqual(self.mod1.created_at, self.mod1.updated_at)
+    def test_pycodestyle(self):
+        style = pycodestyle.StyleGuide(quiet=True)
+        self.assertEqual(
+            style.check_files(['models/base_model.py']).total_errors,
+            0,
+            "Found code style errors (and warnings)."
+        )
 
     def test_id_initialization(self):
+        self.assertTrue(self.mod1.id, exists)
+        self.assertTrue(self.mod2.id, exists)
+        self.assertTrue(type(self.mod1.id) is str)
+        self.assertTrue(type(self.mod2.id) is str)
         self.assertEqual(len(self.mod1.id), 36)
+        self.assertEqual(len(self.mod2.id), 36)
+        self.assertNotEqual(self.mod1.id, self.mod2.id)
+
+    def test_created_at_initialization(self):
+        self.assertTrue(self.mod1.created_at, exists)
+        self.assertTrue(self.mod2.created_at, exists)
+        self.assertNotEqual(self.mod1.created_at, self.mod2.created_at)
+
+    def test_updated_at_initialization(self):
+        self.assertTrue(self.mod1.updated_at, exists)
+        self.assertTrue(self.mod2.updated_at, exists)
+        self.assertEqual(self.mod1.created_at, self.mod1.updated_at)
+        self.assertEqual(self.mod2.created_at, self.mod2.updated_at)
+        self.assertNotEqual(self.mod1.updated_at, self.mod2.updated_at)
 
 
 class TestBaseModel_str(unittest.TestCase):
@@ -43,8 +64,19 @@ class TestBaseModel_str(unittest.TestCase):
     def test_base_model_str(self):
         self.assertEqual(
             str(self.mod1),
-            f"[{self.mod1.__class__.__name__}] \
-({self.mod1.id}) {self.mod1.__dict__}"
+            "[{}] ({}) {}".format(
+                self.mod1.__class__.__name__,
+                self.mod1.id,
+                self.mod1.__dict__,
+            )
+        )
+        self.assertEqual(
+            self.mod1.__str__(),
+            "[{}] ({}) {}".format(
+                self.mod1.__class__.__name__,
+                self.mod1.id,
+                self.mod1.__dict__,
+            )
         )
 
 
@@ -52,6 +84,7 @@ class TestBaseModel_save(unittest.TestCase):
     """ BaseModel save method tests """
     def setUp(self):
         self.mod1 = BaseModel()
+        self.mod1.save()
 
     def tearDown(self):
         del self.mod1
@@ -60,11 +93,16 @@ class TestBaseModel_save(unittest.TestCase):
         with self.assertRaises(TypeError):
             self.mod1.save(1)
 
+    def test_save(self):
+        self.assertNotEqual(self.mod1.updated_at, self.mod1.created_at)
+        self.assertTrue(self.mod1.created_at < self.mod1.updated_at)
+
 
 class TestBaseModel_to_dict(unittest.TestCase):
     """ BaseModel to_dict method tests """
     def setUp(self):
         self.mod1 = BaseModel()
+        self.mod1_dict = self.mod1.to_dict()
 
     def tearDown(self):
         del self.mod1
@@ -72,6 +110,47 @@ class TestBaseModel_to_dict(unittest.TestCase):
     def test_to_dict_with_argument(self):
         with self.assertRaises(TypeError):
             self.mod1.to_dict(1)
+
+    def test_to_dict_success(self):
+        self.assertTrue(self.mod1_dict, exists)
+        self.assertTrue(type(self.mod1_dict) is dict)
+
+    def test_to_dict_keys(self):
+        self.assertIn(
+            '__class__',
+            self.mod1_dict
+        )
+        self.assertIn(
+            'created_at',
+            self.mod1_dict
+        )
+        self.assertIn(
+            'updated_at',
+            self.mod1_dict
+        )
+        self.assertIn(
+            'id',
+            self.mod1_dict
+        )
+
+    def test_to_dict_values(self):
+        self.assertEqual(
+            self.mod1_dict['__class__'],
+            'BaseModel'
+        )
+        self.assertEqual(
+            self.mod1_dict['__class__'],
+            self.mod1.__class__.__name__
+        )
+        self.assertEqual(
+            self.mod1_dict['created_at'],
+            self.mod1.created_at.isoformat()
+        )
+        self.assertEqual(
+            self.mod1_dict['updated_at'],
+            self.mod1.updated_at.isoformat()
+        )
+        self.assertEqual(self.mod1_dict['id'], self.mod1.id)
 
 
 if __name__ == "__main__":
