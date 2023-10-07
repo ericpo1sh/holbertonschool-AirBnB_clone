9dfe451fbbf24b051955c63f6f25b0490960d09e
ericpo1sh <ericpo1sh@gmail.com>
2023-10-07 12:30:22 -0700

    Console def added, EOF, Quit, EmptyLine methods added to HBNBCommand Class

--- console.py
+++ console.py
@@ -4,8 +4,21 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """ placeholder """
-    pass
+    """ Initializing infinite loop and prompt to display """
+    prompt = '(hbnb) '
+
+    def do_emptyline(self):
+        """ If line is empty, do nothing """
+        pass
+
+    def do_EOF(self):
+        """ Quits upon receiving EOF as input """
+        print()
+        return True
+
+    def do_quit(self):
+        """ Quit command to exit the program """
+        return True
 
 
 if __name__ == "__main__":
