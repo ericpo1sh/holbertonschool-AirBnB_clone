59ba9974958b85e5e4f75724f704bcfb5f6620dd
O-01 <sa.an-s@yandex.com>
2023-10-09 11:42:00 -0500

    Added unittests for models/{amenity, city, place, review, state, user}

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,1 +1,62 @@
 #!/usr/bin/python3
+""" unittest module containing tests for Review subclass of BaseModel """
+import os
+import unittest
+import pycodestyle
+from models import storage
+from models.review import Review
+
+
+class TestReview_class(unittest.TestCase):
+    """ Review formatting & initialization tests """
+    @classmethod
+    def setUp(self):
+        self.review1 = Review()
+        self.review2 = Review()
+        self.review3 = Review(**self.review1.to_dict())
+        storage.save()
+
+    @classmethod
+    def tearDown(self):
+        del self.review1
+        del self.review2
+        del self.review3
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+
+    def test_doc_string(self):
+        self.assertTrue(len(Review.__doc__) > 0)
+
+    def test_pycodestyle(self):
+        style = pycodestyle.StyleGuide(quiet=True)
+        self.assertEqual(
+            style.check_files(['models/review.py']).total_errors,
+            0,
+            "Found code style errors (and warnings)."
+        )
+
+    def test_class_attribute_initialization(self):
+        self.assertTrue(type(self.review1.place_id) is str)
+        self.assertTrue(type(self.review1.user_id) is str)
+        self.assertTrue(type(self.review1.text) is str)
+        self.assertTrue(type(self.review2.place_id) is str)
+        self.assertTrue(type(self.review2.user_id) is str)
+        self.assertTrue(type(self.review2.text) is str)
+        self.assertTrue(type(self.review3.place_id) is str)
+        self.assertTrue(type(self.review3.user_id) is str)
+        self.assertTrue(type(self.review3.text) is str)
+        self.assertEqual(self.review1.place_id, "")
+        self.assertEqual(self.review1.user_id, "")
+        self.assertEqual(self.review1.text, "")
+        self.assertEqual(self.review2.place_id, "")
+        self.assertEqual(self.review2.user_id, "")
+        self.assertEqual(self.review2.text, "")
+        self.assertEqual(self.review3.place_id, "")
+        self.assertEqual(self.review3.user_id, "")
+        self.assertEqual(self.review3.text, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,1 +1,50 @@
 #!/usr/bin/python3
+""" unittest module containing tests for State subclass of BaseModel """
+import os
+import unittest
+import pycodestyle
+from models import storage
+from models.state import State
+
+
+class TestState_class(unittest.TestCase):
+    """ State formatting & initialization tests """
+    @classmethod
+    def setUp(self):
+        self.state1 = State()
+        self.state2 = State()
+        self.state3 = State(**self.state1.to_dict())
+        storage.save()
+
+    @classmethod
+    def tearDown(self):
+        del self.state1
+        del self.state2
+        del self.state3
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+
+    def test_doc_string(self):
+        self.assertTrue(len(State.__doc__) > 0)
+
+    def test_pycodestyle(self):
+        style = pycodestyle.StyleGuide(quiet=True)
+        self.assertEqual(
+            style.check_files(['models/state.py']).total_errors,
+            0,
+            "Found code style errors (and warnings)."
+        )
+
+    def test_class_attribute_initialization(self):
+        self.assertTrue(type(self.state1.name) is str)
+        self.assertTrue(type(self.state2.name) is str)
+        self.assertTrue(type(self.state3.name) is str)
+        self.assertEqual(self.state1.name, "")
+        self.assertEqual(self.state2.name, "")
+        self.assertEqual(self.state3.name, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
